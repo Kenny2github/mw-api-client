b19e5d1fe3640fa4b23362bde2d285b7f24afcff
Kenny2github <kenny2minecraft@gmail.com>
2019-04-27 10:40:04 +0800

    Full release to push security

--- mw_api_client/__init__.py
+++ mw_api_client/__init__.py
@@ -97,7 +97,7 @@ from .excs import WikiError, WikiWarning, EditConflict, catch
 from .misc import Tag, RecentChange, GenericData
 from .qyoo import Queue
 
-__version__ = '3.2.0a3'
+__version__ = '3.2.0'
 
 __all__ = [
     'EditConflict',
